0af8e7707c916ca55ef543c28db745c056c32394
TrueNerdDev <deuscontramundum@yandex.ru>
2022-04-30 22:45:17 +0300

    create form

--- cities/views.py
+++ cities/views.py
@@ -9,13 +9,13 @@ __all__ = (
 
 
 def home(request, pk=None):
-    if pk:
-        #city = City.objects.filter(id=pk).first()
-        #city = City.objects.get(id=pk)
-        city = get_object_or_404(City, id=pk)
-
-        context = {'object': city}
-        return render(request, 'cities/detail.html', context)
+    # if pk:
+    #     #city = City.objects.filter(id=pk).first()
+    #     #city = City.objects.get(id=pk)
+    #     # city = get_object_or_404(City, id=pk)
+    #     #
+    #     # context = {'object': city}
+    #     # return render(request, 'cities/detail.html', context)
     qs = City.objects.all()
     context = {'objects_list': qs}
     return render(request, 'cities/home.html', context)
