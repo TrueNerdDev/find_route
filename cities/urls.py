0ded275caf5371bae539a55b83b4e54b1f53fddd
TrueNerdDev <deuscontramundum@yandex.ru>
2022-05-01 00:06:59 +0300

    created form for delete cities

--- cities/urls.py
+++ cities/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
 from cities.views import *
+from cities.views import CityDeleteView
 
 urlpatterns = [
     path('', home, name='home'),
     path('detail/<int:pk>/', CityDetailView.as_view(), name='detail'),
     path('add/', CityCreateView.as_view(), name='create'),
     path('update/<int:pk>/', CityUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>/', CityDeleteView.as_view(), name='delete'),
+
 ]
