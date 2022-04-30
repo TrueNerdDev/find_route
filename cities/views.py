2cf9487118893a77e37f6d84aec6d833008711bf
TrueNerdDev <deuscontramundum@yandex.ru>
2022-04-30 23:45:56 +0300

    created adding and editing cities

--- cities/views.py
+++ cities/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import DetailView
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, CreateView, UpdateView
 
 from cities.forms import HtmlForm, CityForm
 from cities.models import City
 
 __all__ = (
-    'home', 'CityDetailView',
+    'home', 'CityDetailView', 'CityCreateView', 'CityUpdateView',
 )
 
 
@@ -27,6 +28,21 @@ def home(request, pk=None):
     context = {'objects_list': qs, 'form': form}
     return render(request, 'cities/home.html', context)
 
+
 class CityDetailView(DetailView):
     queryset = City.objects.all()
     template_name = 'cities/detail.html'
+
+
+class CityCreateView(CreateView):
+    model = City
+    form_class = CityForm
+    template_name = 'cities/create.html'
+    success_url = reverse_lazy('cities:home')
+
+
+class CityUpdateView(UpdateView):
+    model = City
+    form_class = CityForm
+    template_name = 'cities/update.html'
+    success_url = reverse_lazy('cities:home')
